35290c74400223c9b4f2c08dc2b7bf67fe9a5e76
Pedro Henrique da Silva <123668978+PHS-01@users.noreply.github.com>
2025-08-20 17:23:56 +0000

    raw: Adicionar arquivos de configurações #10

---
+++ include/config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Declaração das variaveis
+extern const char *PROJECT_VERSION;
+extern const char *PROJECT_ASCII_ART[];
+extern const int LENGTH_ASCII_ART;
+
+#endif
