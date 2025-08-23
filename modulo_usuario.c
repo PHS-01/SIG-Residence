3462391093e21105180e5d7787aa5d91e9e3f231
G-Henri <gleydson.silva.716@ufrn.edu.br>
2025-08-23 11:26:50 -0300

    Função de Excluir Cliente adicionada - Sugestão de update: Verificar através do cpf os dados do cliente para confirmação de sua exclusão e deletar seus dados posteriormente

--- modulo_usuario.c
+++ modulo_usuario.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h> // para usar system()
 
 // Função principal - Módulo Usuário
 int user(void) {
@@ -49,16 +48,16 @@ void login_user(char nome[], char cpf[], char data_nascimento[], char matricula[
 
     printf("[✔] - Usuário cadastrado com sucesso!\n");
 
-    printf("Pressione ENTER para continuar...");
-    getchar(); getchar(); // pausa
+    printf("Pressione ENTER para sair...");
+    getchar(); getchar();
 }
 
 // Função - Verificar Usuário
 void check_user(char nome[], char cpf[], char data_nascimento[], char matricula[], char email[], char telefone[], char endereco[]) {
     printf("╔══════════════════════════════════════════╗\n");
-    printf("║            VERIFICAR_USUÁRIO             ║\n"); 
+    printf("║            VERIFICAR_USUÁRIO             ║\n"); // Adicionar o CPF para verificar os dados de cada usuário
     printf("╚══════════════════════════════════════════╝\n");
-    printf("[o] - Nome: %s\n", nome);
+    printf("[o] - Nome: %s\n", nome); 
     printf("[o] - CPF: %s\n", cpf);
     printf("[o] - Data de Nascimento: %s\n", data_nascimento);
     printf("[o] - Matrícula: %s\n",matricula); 
@@ -67,8 +66,75 @@ void check_user(char nome[], char cpf[], char data_nascimento[], char matricula[
     printf("[o] - Endereço: %s\n", endereco);
     
     printf("Pressione ENTER para sair...");
-    getchar();
-    getchar();
+    getchar(); getchar();
 }
 
+// Função - Alterar Usuário
+void change_user(char nome[], char cpf[], char data_nascimento[], char matricula[], char email[], char telefone[], char endereco[]) {
+    printf("╔══════════════════════════════════════════╗\n");
+    printf("║              ALTERAR_USUÁRIO             ║\n"); 
+    printf("╚══════════════════════════════════════════╝\n");
+    printf("[o] - Digite seus novos dados: \n"); // Futuramente adicionar o CPF e exibir os dados para alteração 
+    
+    printf("[+] - Nome: ");
+    scanf(" %[^\n]", nome);
 
+    printf("[+] - CPF: ");
+    scanf(" %[^\n]", cpf);
+
+    printf("[+] - Data de Nascimento: ");
+    scanf(" %[^\n]", data_nascimento);
+
+    printf("[+] - Matrícula: ");
+    scanf(" %[^\n]", matricula);
+
+    printf("[+] - E-mail: ");
+    scanf(" %[^\n]", email);
+
+    printf("[+] - Telefone: ");
+    scanf(" %[^\n]", telefone);
+
+    printf("[+] - Endereço: ");
+    scanf(" %[^\n]", endereco);
+
+    printf("[✔] - Dados alterados com sucesso!\n");
+
+    printf("Pressione ENTER para sair...");
+    getchar(); getchar();
+}
+
+// Função - Excluir Usuário
+void delete_user(char nome[], char cpf[], char data_nascimento[], char matricula[], char email[], char telefone[], char endereco[]) {
+    char op_delete;
+
+    printf("╔══════════════════════════════════════════╗\n");
+    printf("║              EXCLUIR_USUÁRIO             ║\n"); 
+    printf("╚══════════════════════════════════════════╝\n");
+    printf("[o] - Insira seu CPF: \n"); // CPF usado para verificação dos dados anexados a ele.
+
+    printf("[o] - Nome: %s\n", nome);
+    printf("[o] - CPF: %s\n", cpf);
+    printf("[o] - Data de Nascimento: %s\n", data_nascimento);
+    printf("[o] - Matrícula: %s\n",matricula); 
+    printf("[o] - E-mail: %s\n", email);
+    printf("[o] - Telefone: %s\n", telefone);
+    printf("[o] - Endereço: %s\n", endereco);
+    
+    printf("[!] - Excluir dados? (S/N)"); // Confirmação para excluir os dados
+    scanf("%c", op_delete);
+
+    if (op_delete == 'S' || op_delete == 's') {
+    printf("[✔] - Usuário excluído com sucesso!\n");
+    }
+
+    else if (op_delete == 'N' || op_delete == 'n') {
+    printf("[i] - Exclusão cancelada.\n");
+    }
+
+    else {
+    printf("[x] - Resposta inválida!\n");
+    }
+
+    printf("Pressione ENTER para sair...");
+    getchar(); getchar();
+}
