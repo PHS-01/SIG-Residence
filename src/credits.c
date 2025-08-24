d6c1e5294cbcfef6a893f5a1096fc2cfab213169
Pedro Henrique da Silva <123668978+PHS-01@users.noreply.github.com>
2025-08-24 17:45:54 +0000

    style: Atualizar tela de Creditos #3

--- src/credits.c
+++ src/credits.c
@@ -2,79 +2,57 @@
 #include <ncurses.h>
 #include <string.h>
 
+#include "screens.h"
+
 void credits(int y, int x)
 {
-    char *titulo[] = {
-        "####################################################",
-        "-----             C R E D I T O S              -----",
-        ">>>    Projeto de Sistema de gestão de despesa   <<<",
-        "####################################################",
-        ""
+    const char *text[] = {
+        "   ******  *******   ******** *******   ** **********   *******    ********",
+        "  **////**/**////** /**///// /**////** /**/////**///   **/////**  **////// ",
+        " **    // /**   /** /**      /**    /**/**    /**     **     //**/**       ",
+        "/**       /*******  /******* /**    /**/**    /**    /**      /**/*********",
+        "/**       /**///**  /**////  /**    /**/**    /**    /**      /**////////**",
+        "//**    **/**  //** /**      /**    ** /**    /**    //**     **        /**",
+        " //****** /**   //**/********/*******  /**    /**     //*******   ******** ",
+        "  //////  //     // //////// ///////   //     //       ///////   ////////  ",
+        "",
+        "",
+        "A U T O R E S",
+        "",
+        "Pedro Henrique da Silva",
+        "Gleydson Henrique Dantas da Silva",
+        "",
+        "O R I E N T A D O R",
+        "",
+        "Flavius da Luz e Gorgônio",
+        "",
+        "I N S T I T U I C A O",
+        "",
+        "UNIVERSIDADE FEDERAL DO RIO GRANDE DO NORTE",
+        "",
+        "F E R R A M E N T A S",
+        "",
+        "Github and Git",
+        "Linguagem: C",
+        "",
+        "P R O J E T O",
+        "",
+        "https://github.com/PHS-01/SIG-Residence.git",
+        "",
+        "",
+        "",
+        "",
+        "[0] Voltar"
     };
 
-    char *autores[] = {
-        "####################################################",
-        "-----              A U T O R E S                ----",
-        ">>>       Gleydson Henrique Dantas da Silva      <<<",
-        ">>>            Pedro Henrique da Silva           <<<",
-        "####################################################",
-        ""
-    };
+    int length_text = sizeof(text) / sizeof(text[0]);
 
-    char *orientador[] = {
-        "-----           O R I E N T A D O R            -----",
-        ">>>           Flavius da Luz e Gorgônio          <<<",
-        "####################################################",
-        ""
-    };
+    char resp;
 
-    char *instituicao[] = {
-        "-----           I N S T I T U I Ç Ã O          -----",
-        ">>> UNIVERSIDADE FEDERAL DO RIO GRANDE DO NORTE  <<<",
-        "####################################################",
-        ""
-    };
+    do 
+    {
+        resp = draw_alert(text, length_text, strlen(text[0]) + 4, 0);
+    } 
+    while (resp != '0');
 
-    char *ferramentas[] = {
-        "---             F E R R A M E N T A S            ---",
-        ">>>                   Github                     <<<",
-        ">>>                    Git                       <<<",
-        ">>>                 Linguagem: C                 <<<",
-        ">>>               Compilador: GCC                <<<",
-        "####################################################",
-        ""
-    };
-
-    char *projeto[] = {
-        "-----             P R O J E T O                -----",
-        ">>> Repositório:                                 <<<",
-        ">>> https://github.com/PHS-01/SIG-Residence.git  <<<",
-        "####################################################",
-        ""
-    };
-
-    // Juntar todas as seções num vetor
-    char **secoes[] = {titulo, autores, orientador, instituicao, ferramentas, projeto};
-    int tamanhos[] = {
-        sizeof(titulo)/sizeof(titulo[0]),
-        sizeof(autores)/sizeof(autores[0]),
-        sizeof(orientador)/sizeof(orientador[0]),
-        sizeof(instituicao)/sizeof(instituicao[0]),
-        sizeof(ferramentas)/sizeof(ferramentas[0]),
-        sizeof(projeto)/sizeof(projeto[0])
-    };
-
-    int h = 2;
-
-    // Printar seção por seção, centralizando
-    for (int s = 0; s < 6; s++) {
-        for (int i = 0; i < tamanhos[s]; i++) {
-            mvprintw(h, (x/2) - (strlen(secoes[s][i]) / 2), "%s", secoes[s][i]);
-            h++;
-        }
-        h += 1;
-    }
-
-    // Pequeno rodapé
-    mvprintw(y-3, 16, "[0] Voltar ao Menu");
 }
