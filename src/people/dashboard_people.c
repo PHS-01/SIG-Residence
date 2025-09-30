9d1d85b4302ce264d404c5fc4875543359c50eee
Paulo André <paulotheprogramador@gmail.com>
2025-09-30 07:56:45 -0300

    feat: Implementa mais 3 atributos de pessoa

--- src/people/dashboard_people.c
+++ src/people/dashboard_people.c
@@ -1,78 +1,58 @@
 #include <stdio.h>
 #include <string.h>
 #include "screens.h"
+#include "people.h"
 
-// Array global de pessoas
-char people[4][50];
+Pessoa people[MAX_PEOPLE];
+int people_count = 0;
 
-// --- Funções auxiliares para desenhar a tela ---
-// (essas você já deve ter em ansi_utils.c / ui_elements.c)
-void clear_screen(void);
-void draw_box(int x, int y, int w, int h);
-void set_cursor(int x, int y);
-
-// Redesenha o painel de pessoas
-static void draw_people_list(void) {
-    printf("\nPessoas cadastradas:\n");
-    for (int i = 0; i < 4; i++) {
-        if (strcmp(people[i], "Vazio") == 0 || people[i][0] == '\0') {
-            printf(" [%d] (vazio)\n", i + 1);
-        } else {
-            printf(" [%d] %s\n", i + 1, people[i]);
-        }
-    }
-}
-
-// Redesenha o menu
-static void draw_options_panel(void) {
-    printf("\nOpções:\n");
-    printf(" [1] Criar Pessoa\n");
-    printf(" [2] Listar Pessoas\n");
-    printf(" [3] Atualizar Pessoa\n");
-    printf(" [4] Deletar Pessoa\n");
-    printf(" [0] Voltar\n");
-}
-
-// --- Menu principal ---
 void dashboard_people(void) {
-    int option = -1;
+    // carrega ao iniciar
+    load_people("people.txt", people, &people_count);
 
-    load_people("people.txt", people);
+    int option = -1;
+    char buf[32];
 
     while (1) {
-        clear_screen();          // limpa a tela
-        draw_people_list();      // mostra lista
-        draw_options_panel();    // mostra menu
+        // limpa tela (usando ANSI; se você tem função clear_screen(), use-a)
+        printf("\033[2J\033[H");
+
+        // mostra lista e opções
+        read_people(people, people_count);
+
+        printf("\nOpções:\n");
+        printf(" [1] Criar Pessoa\n");
+        printf(" [2] Atualizar Pessoa\n");
+        printf(" [3] Deletar Pessoa\n");
+        printf(" [0] Voltar\n");
+
         printf("\nEscolha uma opção: ");
-        if (scanf("%d", &option) != 1) {
-            while (getchar() != '\n'); // limpa stdin
-            continue;
-        }
-        while (getchar() != '\n'); // limpa buffer
+        if (!fgets(buf, sizeof(buf), stdin)) break;
+        option = atoi(buf);
 
         switch (option) {
             case 1:
-                create_people(people);
-                save_people("people.txt", people);
+                create_people(people, &people_count);
+                save_people("people.txt", people, people_count);
                 break;
             case 2:
-                read_people(people);
+                update_people(people, people_count);
+                save_people("people.txt", people, people_count);
                 break;
             case 3:
-                update_people(people);
-                save_people("people.txt", people);
-                break;
-            case 4:
-                delete_people(people);
-                save_people("people.txt", people);
+                delete_people(people, &people_count);
+                save_people("people.txt", people, people_count);
                 break;
             case 0:
-                return; // volta ao menu anterior
+                // salvar antes de sair da tela, só por segurança
+                save_people("people.txt", people, people_count);
+                return;
             default:
                 printf("Opção inválida.\n");
+                break;
         }
 
         printf("\nPressione ENTER para continuar...");
-        getchar();
+        fgets(buf, sizeof(buf), stdin);
     }
 }
