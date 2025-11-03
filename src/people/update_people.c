a5963ba15bb3844f6bdc1dbe1abe55c30a30ba2e
Paulo André <paulotheprogramador@gmail.com>
2025-11-03 14:40:08 -0300

    feat: Adiciona validação nos updates

--- src/people/update_people.c
+++ src/people/update_people.c
@@ -5,6 +5,7 @@
 #include "terminal_control.h"
 #include "controllers.h"
 #include "config.h"
+#include "validations.h"
 
 void update_people_ui() {
     People updated_person;
@@ -27,41 +28,73 @@ void update_people_ui() {
     print_people_detail(&existing_person);
     printf("\n");
 
-    printf("\nDeixe em branco para manter o valor atual.\n\n");
+    print_info("Deixe em branco para manter o valor atual.\n");
     
     char temp[100];
     
-    // Nome
+    // Nome - com validação
     printf("Nome atual: %s\n", existing_person.name);
     read_string_input("Novo nome: ", temp, sizeof(temp));
     if (strlen(temp) > 0) {
+        do {
+            if (!is_valid_string(temp, sizeof(updated_person.name))) {
+                print_error("Nome não pode estar vazio e deve ter até 99 caracteres.");
+                read_string_input("Novo nome: ", temp, sizeof(temp));
+            } else {
+                break;
+            }
+        } while (1);
         strcpy(updated_person.name, temp);
     } else {
         strcpy(updated_person.name, existing_person.name);
     }
     
-    // Data de nascimento
+    // Data de nascimento - com validação
     printf("Data de nascimento atual: %s\n", existing_person.birth_date);
     read_string_input("Nova data de nascimento: ", temp, sizeof(temp));
     if (strlen(temp) > 0) {
+        do {
+            if (!validation_date(temp)) {
+                print_error("Data inválida! Use o formato dd/mm/aaaa e insira uma data real.");
+                read_string_input("Nova data de nascimento: ", temp, sizeof(temp));
+            } else {
+                break;
+            }
+        } while (1);
         strcpy(updated_person.birth_date, temp);
     } else {
         strcpy(updated_person.birth_date, existing_person.birth_date);
     }
     
-    // Email
+    // Email - com validação
     printf("Email atual: %s\n", existing_person.email);
     read_string_input("Novo email: ", temp, sizeof(temp));
     if (strlen(temp) > 0) {
+        do {
+            if (!is_valid_email(temp)) {
+                print_error("Email inválido! Certifique-se de que contém '@' e '.' e está no formato correto.");
+                read_string_input("Novo email: ", temp, sizeof(temp));
+            } else {
+                break;
+            }
+        } while (1);
         strcpy(updated_person.email, temp);
     } else {
         strcpy(updated_person.email, existing_person.email);
     }
     
-    // Telefone
+    // Telefone - com validação
     printf("Telefone atual: %s\n", existing_person.phone);
     read_string_input("Novo telefone: ", temp, sizeof(temp));
     if (strlen(temp) > 0) {
+        do {
+            if (!is_valid_phone(temp)) {
+                print_error("Telefone inválido! Use apenas números, '+' e '-', com 9 a 20 caracteres.");
+                read_string_input("Novo telefone: ", temp, sizeof(temp));
+            } else {
+                break;
+            }
+        } while (1);
         strcpy(updated_person.phone, temp);
     } else {
         strcpy(updated_person.phone, existing_person.phone);
