f5c89fb879cc0f83ac7c8d514750c903351e2054
Paulo André <paulotheprogramador@gmail.com>
2025-11-03 14:32:11 -0300

    feat: Adiciona residence nas validações

--- include/validations.h
+++ include/validations.h
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include "people.h"
 #include "finance.h"
+#include "residence.h"
 
 // Valida se uma string é uma data válida no formato "dd/mm/yyyy"
 bool validation_date(const char *date);
@@ -20,10 +21,22 @@ bool is_valid_string(const char *str, size_t max_len);
 // Valida se um valor numérico é maior ou igual a zero
 bool is_valid_value(float value);
 
+// Valida CEP
+bool is_valid_cep(const char *cep);
+
+// Valida número da residência
+bool is_valid_residence_number(int number);
+
+// Valida estado (sigla)
+bool is_valid_state(const char *state);
+
 // Valida uma struct Finance
 bool is_valid_finance(const Finance *finance);
 
 // Valida uma struct People
 bool is_valid_people(const People *person);
+
+// Valida uma struct Residence
+bool is_valid_residence(const Residence *residence);
 
 #endif
