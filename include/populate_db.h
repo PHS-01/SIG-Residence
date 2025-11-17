cc87522d11c36954ed52fbfdf3e9f8150f0adf2c
Paulo André <paulotheprogramador@gmail.com>
2025-11-17 14:12:34 -0300

    feat: cria populador de banco de dados

---
+++ include/populate_db.h
@@ -0,0 +1,11 @@
+#ifndef POPULATE_DB_H
+#define POPULATE_DB_H
+
+void populate_all_data(void);
+void populate_people_data(void);
+void populate_residence_data(void);
+void populate_finance_data(void);
+void show_populate_menu(void);
+void show_statistics(void);
+
+#endif
