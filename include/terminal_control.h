f7682f2a8d36c68ac01a2862304c242cfd430275
Paulo André <paulotheprogramador@gmail.com>
2025-12-03 22:52:39 -0300

    refactor: Organiza prototipos de funcoes de terminal

--- include/terminal_control.h
+++ include/terminal_control.h
@@ -4,6 +4,20 @@
 #include <stddef.h> // Para size_t
 #include <stdarg.h> // Para va_list
 
+// Funções de entrada
+void clear_input_buffer();
+int read_int_input(const char* prompt, int* value);
+int read_string_input(const char* prompt, char* buffer, size_t size);
+int read_float_input(const char* prompt, float* value);
+void wait_for_enter(void);
+
+// Funções de output (agora variádicas)
+void print_success(const char* message, ...);
+void print_error(const char* message, ...);
+void print_warning(const char* message, ...);
+void print_info(const char* message, ...);
+
+
 // Cores básicas ANSI
 enum Color {
     MY_COLOR_BLACK = 30,
@@ -34,11 +48,6 @@ enum Color {
 #define COLOR_CYAN    "\033[36m"
 #define COLOR_WHITE   "\033[37m"
 
-// Funções de output (agora variádicas)
-void print_success(const char* message, ...);
-void print_error(const char* message, ...);
-void print_warning(const char* message, ...);
-void print_info(const char* message, ...);
 
 // ANSI Utils
 void move_cursor(int, int);
@@ -53,12 +62,13 @@ void reset_text_color(void);
 
 
 // Termios Utils
-char get_keypress(void);
-void disable_raw_mode(void);
+void save_terminal_state();
 void enable_raw_mode(void);
+void disable_raw_mode(void);
+char get_keypress(void);
 void restore_terminal(void);
-void set_terminal_size(int, int);
-void update_terminal_size(int*, int*);
+void update_terminal_size(int *last_rows, int *last_cols);
+void set_terminal_size(int rows, int cols);
 
 // Input Utils
 void clear_input_buffer(void);
